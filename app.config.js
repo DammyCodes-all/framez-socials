afdf91d9e23eba4f679367985d38adb2d5d9b692
DammyCodes-all <harliarmeen@gmail.com>
2025-11-11 11:01:31 +0100

    feat: update app configuration for Supabase integration, add EAS build settings, and include expo-updates dependency

--- app.config.js
+++ app.config.js
@@ -4,9 +4,18 @@ module.exports = ({ config }) => {
   return {
     ...config,
     expo: {
+      ...(config.expo || {}),
       extra: {
+        ...(config.expo?.extra || {}),
         SUPABASE_URL: process.env.SUPABASE_URL,
         SUPABASE_KEY: process.env.SUPABASE_KEY,
+        eas: {
+          projectId: "697cf503-179d-4074-8edf-34d12b8fbcc8",
+        },
+      },
+      android: {
+        ...(config.expo?.android || {}),
+        package: "com.aluminate.framezsocials",
       },
     },
   };
